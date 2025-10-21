c70570717d0fc5cb53ea16144dc80028814add88
Nitesh Singh <niteshk6720@gmail.com>
2025-10-21 07:55:11 +0000

    can you make the background gradient something like this?  and make all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -27,7 +27,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased noisy-bg">
         {children}
         <Toaster />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ import Footer from '@/components/portfolio/Footer';
 
 export default function Home() {
   return (
-    <div className="relative mx-auto max-w-7xl bg-background text-foreground">
-      <div className="pointer-events-none absolute left-1/2 top-0 h-[400px] w-[800px] -translate-x-1/2 bg-[radial-gradient(40%_40%_at_50%_0%,_rgba(200,100,255,0.1)_0%,_rgba(255,255,255,0)_100%)]"></div>
+    <div className="relative mx-auto max-w-7xl">
       <div className="container mx-auto px-6 md:px-12 lg:px-16">
         <Header />
         <main>
